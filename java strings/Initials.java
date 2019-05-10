5c11782539a1abd44b90c791464085ffde1f80eb
K Poorna chandra rao <50017827+Poorna25@users.noreply.github.com>
2019-05-10 14:38:02 +0530

    Update Initials.java

--- java strings/Initials.java
+++ java strings/Initials.java
@@ -2,12 +2,21 @@ import java.util.Scanner;
 public class Initials
 {
 	 public static void main(String[] args) {
+	 	String str;
+	 	char ch;
+	 	int len;
 	 	Scanner scan = new Scanner(System.in);
-
-	 	String first = scan.nextLine();
-	 	String last = scan.nextLine();
-
-	 	String initials = first.substring(0,1) + "." + last.substring(0,1);
-	 	System.out.println(initials);
+	 	System.out.println("enter the input string");
+        str = scan.nextLine();
+        str = " " + str;
+        str = str.toUpperCase();
+        len = str.length();
+        System.out.println("output =");
+        for(int i = 0 ; i < len ; i++){
+        	ch = str.charAt(i);
+        	if(ch == ' ')
+        		System.out.println(str.charAt(i+1) + " ");
+        }
+	 	
 	 }
 }
