87535e1cc3de998ca1588fb78260b57b977c048c
K Poorna chandra rao <50017827+Poorna25@users.noreply.github.com>
2019-05-28 17:49:28 +0530

    Update IProductService.java

--- takeHome/src/com/ibm/takehome/service/IProductService.java
+++ takeHome/src/com/ibm/takehome/service/IProductService.java
@@ -4,7 +4,7 @@ import com.ibm.takehome.bean.Product;
 import com.ibm.takhome.problem.NullException;
 
 public interface IProductService {
-	// interface get and set methods
+
 	Product getProductDetails(Integer productId) throws NullException;
 	boolean setProductDetails(Product productToEnlist);
 }
