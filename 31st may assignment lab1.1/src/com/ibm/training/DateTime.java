132d6019f6e71357159b95711245d56ea8497531
K Poorna chandra rao <50017827+Poorna25@users.noreply.github.com>
2019-05-31 17:21:30 +0530

    Add files via upload

---
+++ 31st may assignment lab1.1/src/com/ibm/training/DateTime.java
@@ -0,0 +1,29 @@
+package com.ibm.training;
+
+import java.io.*;
+import java.util.*;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/datetime")
+public class DateTime extends HttpServlet {
+             
+	      
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		Date d = new Date();
+		
+        out.println(d);
+}
+}
